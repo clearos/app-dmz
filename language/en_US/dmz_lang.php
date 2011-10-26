0e1d00d3c048f07f5de7eb7db31e3b296036d021
Ben Chambers <bchambers@clearfoundation.com>
2011-10-26 03:37:13 +0000

    - Moved DMZ lang tag to firewall - Expose get_domains_list in Metadata library

--- language/en_US/dmz_lang.php
+++ language/en_US/dmz_lang.php
@@ -6,5 +6,4 @@ $lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ to LAN) Connection';
 $lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ to LAN) Connections';
 $lang['dmz_incoming_connection'] = 'DMZ Incoming Connection';
 $lang['dmz_incoming_connections'] = 'DMZ Incoming Connections';
-$lang['dmz_all_protocols_and_ports'] = 'All Protocols and Ports';
 $lang['dmz_network_not_configured'] = 'A network interface with a designated role of DMZ could not be found.  A minimum of three network interfaces are required, one interface having the DMZ designation.  Navigate to "Network >> IP Settings" if you have not setup the 3rd NIC as a DMZ.';
