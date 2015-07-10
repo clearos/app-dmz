c46d264383c7bce0f905091e7584197fadde2630
pcbaldwin <pbaldwin@clearfoundation.com>
2015-07-10 13:13:41 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'dmz';
-$app['version'] = '2.0.19';
+$app['version'] = '2.1.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-dmz.spec
+++ packaging/app-dmz.spec
@@ -1,7 +1,7 @@
 
 Name: app-dmz
 Epoch: 1
-Version: 2.0.19
+Version: 2.1.6
 Release: 1%{dist}
 Summary: DMZ Firewall
 License: GPLv3
