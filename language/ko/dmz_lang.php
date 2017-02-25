5a4ffe105a592e0d812d4b28e62171de05059297
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:50:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = '비무장 지대의 응용 프로그램은 비무장 지대에서 시스템에 방화벽 도구를 제공합니다. 이러한 웹, 이메일, VoIP 시스템 고위험 외부 서비스를 분리하는 것은 네트워크의 보안을 향상시킬 수있다.';
+$lang['dmz_app_name'] = 'DMZ 방화벽';
+$lang['dmz_incoming_connection'] = 'DMZ 들어오는 연결';
+$lang['dmz_incoming_connections'] = 'DMZ 들어오는 연결';
+$lang['dmz_network_not_configured'] = 'DMZ의 지정된 역할이있는 네트워크 인터페이스를 찾을 수 없습니다.';
+$lang['dmz_pinhole_connection'] = 'DMZ 핀홀 (LAN에 DMZ) 연결';
+$lang['dmz_pinhole_connections'] = '(LAN에 DMZ) DMZ 핀홀 연결';
