9ff6c4743205d01f28fb64b9c92f50a283edf837
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:52:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'यो DMZ अनुप्रयोग डे-militarized क्षेत्रमा प्रणाली लागि फायरवाल उपकरण प्रदान गर्दछ। यस्तो वेब, ई-मेल र वीओआईपी सिस्टम उच्च जोखिम बाह्य सेवाहरू अलग आफ्नो नेटवर्कको सुरक्षा सुधार गर्न सक्नुहुन्छ।';
+$lang['dmz_app_name'] = 'DMZ फायरवल';
+$lang['dmz_incoming_connection'] = 'DMZ आगमन जडान';
+$lang['dmz_incoming_connections'] = 'DMZ आगमन जडान';
+$lang['dmz_network_not_configured'] = 'DMZ को एक डिजाइन भूमिका संग नेटवर्क इन्टरफेस भेट्टाउन सकिएन।';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (लैन गर्न DMZ) जडान';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ लैन गर्न) जडान';
