af65ff4014c2e529c0062ecd56121301acdbe115
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:51:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ programa suteikia užkardos įrankiai sistemų de sukarinta zonoje. Skiriamieji didelės rizikos išorės paslaugas, tokias kaip interneto, e-mail, ir VoIP sistemos gali pagerinti jūsų tinklo saugumą.';
+$lang['dmz_app_name'] = 'DMZ užkarda';
+$lang['dmz_incoming_connection'] = 'DMZ Priimamojo Ryšio';
+$lang['dmz_incoming_connections'] = 'DMZ priimamus ryšius';
+$lang['dmz_network_not_configured'] = 'Tinklo sąsaja su paskirtą vaidmenį DMZ negalima rasti.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ LAN) Jungtis';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ LAN) jungtis';
