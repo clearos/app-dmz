1a008a9e42e777d2bfa51dc15d7b7498e9846b50
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:49:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'The DMZ ngwa na-enye firewall ngwá ọrụ maka usoro na de-agha mpaghara. -Akpa iche elu n&#39;ihe ize ndụ ọrụ mpụga dị ka web, e-mail na VoIP usoro nwere ike mma na nche nke netwọk gị.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ abata na Connection';
+$lang['dmz_incoming_connections'] = 'DMZ abata na njikọ';
+$lang['dmz_network_not_configured'] = 'A netwọk interface na a ẹkedọhọde ọrụ nke DMZ nweghị onye hụrụ.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ ka LAN) Njikọ';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ ka LAN) Connections';
