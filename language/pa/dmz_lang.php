3e7999f21b0a410d27400a19bcf71bef2de7b0f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:52:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'ਬਾਰਡਰ ਐਪ de-ਸੈਨਿਕ ਖੇਤਰ &#39;ਚ ਸਿਸਟਮ ਲਈ ਫਾਇਰਵਾਲ ਟੂਲ ਦਿੰਦਾ. ਅਜਿਹੇ ਵੈੱਬ, ਈ-ਮੇਲ ਅਤੇ VoIP ਸਿਸਟਮ ਦੇ ਤੌਰ ਤੇ ਉੱਚ ਖਤਰੇ ਨੂੰ ਬਾਹਰੀ ਸੇਵਾਵ ਨੂੰ ਵੱਖਰਾ ਆਪਣੇ ਨੈੱਟਵਰਕ ਦੀ ਸੁਰੱਖਿਆ ਵਿੱਚ ਸੁਧਾਰ ਕਰ ਸਕਦੇ ਹੋ.';
+$lang['dmz_app_name'] = 'ਬਾਰਡਰ ਫਾਇਰਵਾਲ';
+$lang['dmz_incoming_connection'] = 'ਬਾਰਡਰ ਆਉਣ-ਵਾਲੇ ਕੁਨੈਕਸ਼ਨ';
+$lang['dmz_incoming_connections'] = 'ਬਾਰਡਰ ਆਉਣ ਵਾਲੇ ਕੁਨੈਕਸ਼ਨ';
+$lang['dmz_network_not_configured'] = 'ਬਾਰਡਰ ਦੇ ਇੱਕ ਮਨੋਨੀਤ ਭੂਮਿਕਾ ਨਾਲ ਇੱਕ ਨੈੱਟਵਰਕ ਇੰਟਰਫੇਸ ਲੱਭਿਆ ਜਾ ਸਕਦਾ ਹੈ.';
+$lang['dmz_pinhole_connection'] = 'ਬਾਰਡਰ Pinhole (LAN ਦਾ ਬਾਰਡਰ) ਕੁਨੈਕਸ਼ਨ';
+$lang['dmz_pinhole_connections'] = 'ਬਾਰਡਰ Pinhole (ਬਾਰਡਰ LAN ਕਰਨ ਲਈ) ਕੁਨੈਕਸ਼ਨ';
