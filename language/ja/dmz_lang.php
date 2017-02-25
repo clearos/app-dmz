4414dfa931ffec4a9c0ab3e3a4253414603508f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:49:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZアプリは非武装地帯内のシステムのファイアウォールのツールを提供します。 このようなウェブ、電子メールやVoIPシステムなどの高リスク外部サービスを分離することにより、ネットワークのセキュリティを向上させることができます。';
+$lang['dmz_app_name'] = 'DMZファイアウォール';
+$lang['dmz_incoming_connection'] = 'DMZ着信接続';
+$lang['dmz_incoming_connections'] = 'DMZ着信接続';
+$lang['dmz_network_not_configured'] = 'DMZの指定された役割を持つネットワークインタフェースが見つかりませんでした。';
+$lang['dmz_pinhole_connection'] = 'DMZピンホール（DMZ LAN）に接続';
+$lang['dmz_pinhole_connections'] = 'DMZピンホール（DMZ LAN）に接続';
