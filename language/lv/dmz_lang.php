ae2e99769d854e23f9f616c7eed63ab041b137bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:51:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ app nodrošina ugunsmūra rīkus sistēmām de militarizētais zonā. Izolējot augsta riska ārējos pakalpojumus, piemēram, interneta, e-pastu un VoIP sistēmu var uzlabot jūsu tīkla drošību.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Ienākošā Connection';
+$lang['dmz_incoming_connections'] = 'DMZ ienākošos savienojumus';
+$lang['dmz_network_not_configured'] = 'Tīkla saskarne ar izraudzīto lomu DMZ nevar atrast.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ uz LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ uz LAN) Connections';
