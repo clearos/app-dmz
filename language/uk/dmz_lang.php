d980937f978b061356b942035c7f90efb2327528
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:56:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Додаток DMZ надає інструменти брандмауера для систем в зоні демілітаризованої. Розділовий з високим рівнем ризику зовнішніх послуг, таких як Інтернет, електронної пошти і системи VoIP може підвищити безпеку вашої мережі.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'ДМЗ вхідного з&#39;єднання';
+$lang['dmz_incoming_connections'] = 'DMZ Вхідні підключення';
+$lang['dmz_network_not_configured'] = 'Мережевий інтерфейс з призначеним ролі ДЗ не може бути знайдений.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ до LAN) Підключення';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ до LAN) Підключення';
