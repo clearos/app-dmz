951ef0c19e37530ec51d29140806080cf44a0105
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:52:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'De DMZ app biedt firewall gereedschappen voor systemen in de de-gemilitariseerd zone. Het isoleren van een hoog risico van externe diensten zoals internet, e-mail en VoIP-systeem kan de beveiliging van uw netwerk te verbeteren.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ binnenkomende verbinding';
+$lang['dmz_incoming_connections'] = 'DMZ Binnenkomende verbindingen';
+$lang['dmz_network_not_configured'] = 'Een netwerk interface met een aangewezen rol van DMZ kon niet worden gevonden.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ naar LAN) Connectie';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ naar LAN) Connections';
