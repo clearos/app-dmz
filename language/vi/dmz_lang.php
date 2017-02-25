97b247db4efe163c7489c889211a3ae7639140e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:56:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Các ứng dụng DMZ cung cấp công cụ tường lửa cho hệ thống trong vùng de-quân sự hóa. Cô lập các dịch vụ bên ngoài có nguy cơ cao như web, e-mail và hệ thống VoIP có thể cải thiện sự an toàn của mạng của bạn.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ kết nối Incoming';
+$lang['dmz_incoming_connections'] = 'DMZ kết nối Incoming';
+$lang['dmz_network_not_configured'] = 'Một giao diện mạng với một vai trò chỉ định của DMZ không thể tìm được.';
+$lang['dmz_pinhole_connection'] = 'DMZ pinhole (DMZ với mạng LAN) kết nối';
+$lang['dmz_pinhole_connections'] = 'DMZ pinhole (DMZ với mạng LAN) kết nối';
