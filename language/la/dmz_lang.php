2015e5cb9ffb40de7961cd3589e2cc64c0727461
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:50:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'A app DMZ provides instrumenta et systemata in Firewall militarized ad de-zone. Web Services externa talis ut altus periculum solitudinem, e-mail, potest emendare in securitatem network system et VoIP.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ invenientes Connection';
+$lang['dmz_incoming_connections'] = 'Iunctio DMZ invenientes';
+$lang['dmz_network_not_configured'] = 'A network interface cum DMZ munus designantur, inveniri non potuerunt.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ ad Lan) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ ad Lan) Iunctio';
