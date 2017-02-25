d1bc7adb01ee74c00e7134e5569dac419a285175
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:48:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'The DMZ app bayar da Tacewar zaɓi kayayyakin aiki, don tsarin a cikin de-yan bindiga a yankin. Isolating high hadarin external ayyuka kamar yanar gizo, e-mail da kuma VoIP tsarin zai iya inganta tsaro na cibiyar sadarwa.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ shigowa Connection';
+$lang['dmz_incoming_connections'] = 'DMZ shigowa Connections';
+$lang['dmz_network_not_configured'] = 'A cibiyar sadarwa dubawa tare da sanya rawar da DMZ ba za a iya samu.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ to LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ to LAN) Connections';
