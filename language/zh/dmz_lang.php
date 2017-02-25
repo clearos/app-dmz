3ae4703226fc3606ddf395657870c3cf973f1407
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:47:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = '在DMZ应用提供了在非军事区系统的防火墙工具。 隔离高风险的外部服务，如Web，电子邮件和VoIP系统可以提高网络的安全性。';
+$lang['dmz_app_name'] = 'DMZ防火墙';
+$lang['dmz_incoming_connection'] = 'DMZ传入连接';
+$lang['dmz_incoming_connections'] = 'DMZ传入连接';
+$lang['dmz_network_not_configured'] = '与DMZ的指定角色的网络接口不能被发现。';
+$lang['dmz_pinhole_connection'] = 'DMZ针孔（DMZ到LAN）连接';
+$lang['dmz_pinhole_connections'] = 'DMZ针孔（DMZ到LAN）连接';
