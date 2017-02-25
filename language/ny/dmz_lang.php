57154b9b45cb7c517160bdd4bc317eb0ac03f0bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:46:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'The app DMZ mabuku okuthandizani makhoma oteteza kachitidwe mu zone de A-zankhondo. Kusakonda chiopsezo ntchito mkulu kunja monga ukonde e-mail ndi VoIP dongosolo kusintha chitetezo cha maukonde anu.';
+$lang['dmz_app_name'] = 'DMZ makhoma oteteza';
+$lang['dmz_incoming_connection'] = 'DMZ Zofika mgwirizano';
+$lang['dmz_incoming_connections'] = 'DMZ Zofika kugwirizana';
+$lang['dmz_network_not_configured'] = 'A mawonekedwe maukonde ndi udindo anasankha wa DMZ sanapezeke.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ kuti LAN) mgwirizano';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ kuti LAN) kugwirizana';
