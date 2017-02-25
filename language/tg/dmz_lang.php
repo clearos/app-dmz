bb12541e4ad7027d9370ddc524ceea5c8fb6f363
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:55:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Дар барнома DMZ таъмин воситаҳои Сипар барои системаҳои дар минтақаи-де низомигардонидашуда аст. Људо хадамоти беруна хавфи баланд ба монанди веб, почтаи электронӣ ва системаи VoIP метавонад ба амнияти шабака шуморо беҳтар кунад.';
+$lang['dmz_app_name'] = 'DMZ Сипар';
+$lang['dmz_incoming_connection'] = 'DMZ Пайвастшавӣ воридотӣ';
+$lang['dmz_incoming_connections'] = 'DMZ Пайвастшавиҳо воридотӣ';
+$lang['dmz_network_not_configured'] = 'A интерфейси шабакаро бо нақши муқарраргардидаи DMZ ёфта намешавад.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ ба LAN) Пайвастшавӣ';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ ба LAN) Пайвастшавиҳо';
