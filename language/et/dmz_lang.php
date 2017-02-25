962b51a8f1a068093bf620ee87f0fca6a91634f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:47:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ app pakub tulemüüri vahendid süsteemide de-militariseeritud tsooni. Isoleerides kõrge riskiga sisseostetavate teenuste nagu web, e-posti ja VoIP süsteemi saab parandada turvalisust oma võrgustikku.';
+$lang['dmz_app_name'] = 'DMZ tulemüür';
+$lang['dmz_incoming_connection'] = 'DMZ sissetulevat ühendust';
+$lang['dmz_incoming_connections'] = 'DMZ sissetulevad ühendused';
+$lang['dmz_network_not_configured'] = 'Võrguliidese koos määratud rolli DMZ ei leitud.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pisiava (DMZ LAN) ühendus';
+$lang['dmz_pinhole_connections'] = 'DMZ Pisiava (DMZ LAN) ühendused';
