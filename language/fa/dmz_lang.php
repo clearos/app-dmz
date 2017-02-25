c2d9e2fd368d6a6e09d0b4ece1c455a44d81ab10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:53:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'برنامه DMZ فراهم می کند ابزار فایروال برای سیستم های در منطقه د نظامی است. جدا خدمات خارجی در معرض خطر مانند وب، ایمیل و سیستم VoIP می تواند امنیت شبکه خود را بهبود بخشد.';
+$lang['dmz_app_name'] = 'DMZ فایروال';
+$lang['dmz_incoming_connection'] = 'DMZ اتصال ورودی';
+$lang['dmz_incoming_connections'] = 'DMZ اتصالات ورودی';
+$lang['dmz_network_not_configured'] = 'یک رابط شبکه با نقش تعیین شده از DMZ تواند یافت نمی شود.';
+$lang['dmz_pinhole_connection'] = 'DMZ pinhole که (DMZ به LAN) اتصال';
+$lang['dmz_pinhole_connections'] = 'DMZ pinhole که (DMZ به LAN) اتصالات';
