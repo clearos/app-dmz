b18b9e505a0d97bbc8167ef0c6d7e959a7c54b5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:51:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Te taupānga DMZ whakarato taputapu pātūahi mō ngā pūnaha i roto i te rohe de-militarized. Faatea tiketike mōrea ratonga waho pērā i te tukutuku, ī-mēra, me te pūnaha VoIP taea te whakapai ake i te haumarutanga o koutou whatunga.';
+$lang['dmz_app_name'] = 'DMZ Pātūahi';
+$lang['dmz_incoming_connection'] = 'DMZ Hononga Taumai';
+$lang['dmz_incoming_connections'] = 'DMZ Tūhononga Taumai';
+$lang['dmz_network_not_configured'] = 'e kore i taea te kitea te atanga whatunga ki te tūranga tohua o DMZ.';
+$lang['dmz_pinhole_connection'] = 'DMZ kōhao (DMZ ki LAN) Hononga';
+$lang['dmz_pinhole_connections'] = 'DMZ kōhao (DMZ ki LAN) Hononga';
