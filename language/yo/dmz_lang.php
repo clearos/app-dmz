83cefc943d4c55f6fe9eaa9b9526a268f08c916a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:57:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Awọn DMZ app pese ogiriina irinṣẹ fun awọn ọna šiše ni de-militarized ibi kan. Isolating ga ewu ita iṣẹ gẹgẹ bi awọn ayelujara, e-mail ki o si Voip eto le mu awọn aabo ti nẹtiwọki rẹ.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ nwọle Asopọ';
+$lang['dmz_incoming_connections'] = 'DMZ nwọle isopọ';
+$lang['dmz_network_not_configured'] = 'A nẹtiwọki ni wiwo pẹlu kan pataki ipa ti DMZ ko le ṣee ri.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ to lan) Asopọ';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ to lan) isopọ';
