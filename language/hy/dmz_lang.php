eb984a7139b0764c8340c2503ad09f7a5dca2967
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:45:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'The DMZ ծրագիրը նախատեսում firewall գործիքներ համակարգերի դե ռազմականացված գոտում: Մեկուսացնելու բարձր ռիսկի արտաքին ծառայություններ, ինչպիսիք են վեբ, e-mail եւ VoIP համակարգը կարող է բարելավել անվտանգությունը ձեր ցանցի.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Ներգնա Connection';
+$lang['dmz_incoming_connections'] = 'DMZ Ներգնա Connections';
+$lang['dmz_network_not_configured'] = 'Ցանցը ինտերֆեյսի հետ նշանակած դերի DMZ չեն կարող գտնվել:';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ է LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ է LAN) Connections';
