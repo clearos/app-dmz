6c97b625572d7ded6db9588873e8c7ad6e53e064
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:49:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Soláthraíonn an app DMZ Uirlisí balla dóiteáin do chórais sa chrios de-militarized. Is féidir le isolating Seirbhísí ardriosca seachtracha ar nós gréasáin, r-phost, agus córas VoIP feabhas slándáil do líonra.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Ceangal Isteach';
+$lang['dmz_incoming_connections'] = 'DMZ Connections Isteach';
+$lang['dmz_network_not_configured'] = 'Ní fhéadfaí comhéadan líonra le ról ainmnithe DMZ a fháil.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ go LAN) Ceangal';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ go LAN) Naisc';
