9be6d128f8e72589fd0521645136086635bdc0a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:47:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ sovellus tarjoaa palomuurin työkaluja järjestelmien de-militarisoitu vyöhyke. Eristämällä suuri riski ulkoisiin palveluihin, kuten web, sähköposti ja VoIP-järjestelmä voi parantaa verkon suojausta.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Tuleva yhteys';
+$lang['dmz_incoming_connections'] = 'DMZ Tulevien yhteyksien';
+$lang['dmz_network_not_configured'] = 'Verkkoliitäntä on nimetty roolia DMZ ei löytynyt.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ LAN) Liitäntä';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ LAN) Liitännät';
