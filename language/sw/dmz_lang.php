afb91aae95da569ac903d64dba5129b740d4d79d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:54:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'programu DMZ hutoa zana firewall kwa mifumo katika ukanda de-wa jeshi. Kuwatenga hatari huduma ya juu ya nje kama vile mtandao, barua pepe na VoIP mfumo wanaweza kuboresha usalama wa mtandao wako.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ yaingia Connection';
+$lang['dmz_incoming_connections'] = 'DMZ yaingia Connections';
+$lang['dmz_network_not_configured'] = 'interface mtandao na jukumu mteule wa DMZ hakuweza kupatikana.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ kwa LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ kwa LAN) Connections';
