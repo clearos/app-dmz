12a732f9e98867fa22f313b9dfe349eb9d453fed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:49:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'A DMZ app nyújt tûzfaleszközt rendszerek a demilitarizált övezetben. Leválasztó magas kockázatú külső szolgáltatásokat, mint például web, e-mail és a VoIP rendszer biztonságának javítása a hálózat.';
+$lang['dmz_app_name'] = 'DMZ tűzfal';
+$lang['dmz_incoming_connection'] = 'DMZ bejövő kapcsolat';
+$lang['dmz_incoming_connections'] = 'DMZ Bejövő kapcsolatok';
+$lang['dmz_network_not_configured'] = 'A hálózati interfész egy kijelölt szerepét DMZ nem található.';
+$lang['dmz_pinhole_connection'] = 'DMZ lyukra (DMZ LAN) kapcsolat';
+$lang['dmz_pinhole_connections'] = 'DMZ lyukra (DMZ LAN) kapcsolatok';
