1e3418cb319ec0c216102f36adae4d9ebd620d3d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:45:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'ويوفر التطبيق DMZ أدوات جدار الحماية لأنظمة في المنطقة المنزوعة السلاح. يمكن عزل الخدمات الخارجية عالية المخاطر مثل الإنترنت والبريد الإلكتروني ونظام الاتصالات عبر بروتوكول الإنترنت تحسين أمن الشبكة.';
+$lang['dmz_app_name'] = 'المنطقة المجردة من السلاح الناري';
+$lang['dmz_incoming_connection'] = 'DMZ اتصال وارد';
+$lang['dmz_incoming_connections'] = 'DMZ الاتصالات الواردة';
+$lang['dmz_network_not_configured'] = 'لم يتم العثور على واجهة الشبكة مع دور المعين من المنطقة المجردة من السلاح.';
+$lang['dmz_pinhole_connection'] = 'DMZ الثقب (DMZ إلى LAN) اتصال';
+$lang['dmz_pinhole_connections'] = 'DMZ الثقب (DMZ إلى LAN) اتصالات';
