2eabb88092b5a69d7163390eecf6ce9a09067554
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:55:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ பயன்பாட்டை இராணுவக் மண்டலத்தில் அமைப்புகள் ஃபயர்வால் கருவிகள் வழங்குகிறது. வலை, மின்னஞ்சல் மற்றும் VoIP அமைப்பு போன்ற அதிக ஆபத்தில் வெளி சேவைகள் பிரித்தெடுத்து உங்கள் நெட்வொர்க் பாதுகாப்பு மேம்படுத்த முடியும்.';
+$lang['dmz_app_name'] = 'DMZ ஃபயர்வால்';
+$lang['dmz_incoming_connection'] = 'DMZ உள்வரும் இணைப்பு';
+$lang['dmz_incoming_connections'] = 'DMZ உள்வரும் இணைப்புகளை';
+$lang['dmz_network_not_configured'] = 'DMZ ஒரு நியமிக்கப்பட்ட பங்கு பிணைய முகப்பு கண்டறிய முடியவில்லை.';
+$lang['dmz_pinhole_connection'] = 'DMZ குண்டூசி துளை (லேன் DMZ) இணைப்பு';
+$lang['dmz_pinhole_connections'] = '(லேன் DMZ) DMZ குண்டூசி துளை இணைப்புகள்';
