f7526594b3514dab932daf6da1c6e55ad5a27908
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:46:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'အဆိုပါမျဇ app ကို de-စစ်တပ်အုပ်ချုပ်ဇုန်အတွက်စနစ်များအတွက် firewall က tools တွေကိုပေးစွမ်းသည်။ ထိုကဲ့သို့သောဝက်ဘ်, e-mail, နှင့် VoIP စနစ်ကအဖြစ်မြင့်မားအန္တရာယ်ပြင်ပဝန်ဆောင်မှုများကိုသီးခြားသင့်ကွန်ယက်၏လုံခြုံရေးတိုးတက်ကောင်းမွန်နိုင်ပါတယ်။';
+$lang['dmz_app_name'] = 'မျဇ Firewall ကို';
+$lang['dmz_incoming_connection'] = 'မျဇဝင်လာသောကော်နက်ရှင်';
+$lang['dmz_incoming_connections'] = 'မျဇဝင်လာသော Connections ကို';
+$lang['dmz_network_not_configured'] = 'မျဇ၏သတ်မှတ်ထားတဲ့အခန်းကဏ္ဍနှင့်အတူတစ်ဦးက network interface ကိုမတှေ့နိုငျတယျ။';
+$lang['dmz_pinhole_connection'] = 'မျဇပေါက်ကိုဖောက်ပေးထားတယ် (မျဇရှိနေရင် LAN မှ) ကော်နက်ရှင်';
+$lang['dmz_pinhole_connections'] = 'မျဇပေါက်ကိုဖောက်ပေးထားတယ် (မျဇရှိနေရင် LAN မှ) Connections ကို';
