f336f1e2c7d601984bbe7e1378f159a9ece30a3e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:47:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ Aplikace poskytuje nástroje pro firewall systémů v de polovojenská zóny. Izolování vysoce rizikové externí služby, jako je web, e-mail a VoIP systému může zlepšit zabezpečení sítě.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Příchozí Připojení';
+$lang['dmz_incoming_connections'] = 'DMZ příchozí připojení';
+$lang['dmz_network_not_configured'] = 'nelze nalézt síťové rozhraní s určeným roli DMZ.';
+$lang['dmz_pinhole_connection'] = 'DMZ dírkové (DMZ do LAN)';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ do LAN) Připojení';
