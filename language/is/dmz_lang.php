934069809f19cc8420c51abec1d81747656f6ae1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:49:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'The DMZ app veitir eldvegg tæki til kerfi í de-hervæddu svæði. Einangra mikil áhætta ytri þjónustu, svo sem eins og vefur, e-mail og VoIP kerfi er hægt að bæta öryggi net.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ inntengingarinnar';
+$lang['dmz_incoming_connections'] = 'DMZ komandi tengingar';
+$lang['dmz_network_not_configured'] = 'A net tengi með tilnefnd hlutverki DMZ fannst ekki.';
+$lang['dmz_pinhole_connection'] = 'DMZ títuprjónshaus (DMZ til LAN) Tenging';
+$lang['dmz_pinhole_connections'] = 'DMZ títuprjónshaus (DMZ að LAN) Tengingar';
