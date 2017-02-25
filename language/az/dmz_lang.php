2ffea24c3852817d6e21c5013a020e2cc3a02cd7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:45:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ app de-hərbi zonasında sistemləri üçün firewall alətləri təmin edir. belə web, e-mail və VoIP sistemi kimi yüksək risk xarici xidmətləri təcrid şəbəkə təhlükəsizliyini inkişaf edə bilər.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Gələn Connection';
+$lang['dmz_incoming_connections'] = 'DMZ Gələn Connections';
+$lang['dmz_network_not_configured'] = 'DMZ bir təyin rolu ilə şəbəkə interface tapılmadı.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhol (LAN DMZ) Connection';
+$lang['dmz_pinhole_connections'] = '(LAN DMZ) DMZ Pinhol Əlaqələri';
