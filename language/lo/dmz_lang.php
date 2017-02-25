4fdd112f24d273e1ba83df4011316bf34d533e7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:50:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'ການ app DMZ ມີເຄື່ອງມືໄຟວໍສໍາຫລັບລະບົບໃນເຂດ de-militarized. ແຍກການບໍລິການພາຍນອກມີຄວາມສ່ຽງສູງເຊັ່ນ: ເວັບໄຊທ໌ e-mail ແລະລະບົບ VoIP ສາມາດປັບປຸງຄວາມປອດໄພຂອງເຄືອຂ່າຍຂອງທ່ານ.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ ເຊື່ອມຕໍ່ມາ';
+$lang['dmz_incoming_connections'] = 'DMZ ການເຊື່ອມຕໍ່ເຂົ້າມາ';
+$lang['dmz_network_not_configured'] = 'ມີການໂຕ້ຕອບເຄືອຂ່າຍທີ່ມີພາລະບົດບາດທີ່ກໍານົດຂອງ DMZ ບໍ່ສາມາດໄດ້ຮັບການພົບເຫັນ.';
+$lang['dmz_pinhole_connection'] = 'DMZ ຮູເຂັມ (DMZ ກັບ LAN) ການເຊື່ອມຕໍ່';
+$lang['dmz_pinhole_connections'] = 'DMZ ຮູເຂັມ (DMZ ກັບ LAN) ການເຊື່ອມຕໍ່';
