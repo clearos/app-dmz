33d60ab4f01ddd4c276c50730067778a915d85b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:56:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ اے پی پی ڈی فوجی زون میں نظام کے لئے فائر وال آلات فراہم. اس طرح کے ویب، ای میل اور ویوآئپی نظام کے اعلی خطرے بیرونی خدمات الگ تھلگ اپنے نیٹ ورک کی سلامتی کو بہتر بنا سکتے ہیں.';
+$lang['dmz_app_name'] = 'DMZ فصیل';
+$lang['dmz_incoming_connection'] = 'DMZ موصولہ کنکشن';
+$lang['dmz_incoming_connections'] = 'DMZ موصولہ کنکشنز';
+$lang['dmz_network_not_configured'] = 'DMZ کی ایک مخصوص کردار کے ساتھ ایک نیٹ ورک انٹرفیس نہیں مل سکی.';
+$lang['dmz_pinhole_connection'] = 'DMZ pinhole کا (LAN کرنے DMZ) کنکشن';
+$lang['dmz_pinhole_connections'] = 'DMZ pinhole کا (LAN کرنے DMZ) کنکشن';
