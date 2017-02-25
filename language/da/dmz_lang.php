3d393523f37d6522e4c27ec6d6a67a0c0a401aa1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:47:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ app giver firewall værktøjer til systemer i de-militariseret zone. Isolering højrisiko eksterne tjenester såsom web, e-mail og VoIP-system kan forbedre sikkerheden i dit netværk.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Indgående Connection';
+$lang['dmz_incoming_connections'] = 'DMZ Indgående forbindelser';
+$lang['dmz_network_not_configured'] = 'Et netværksinterface med en udpeget rolle DMZ kunne ikke findes.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ til LAN) Connected';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ til LAN) Tilslutninger';
