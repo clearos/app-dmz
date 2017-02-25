c8494c4eb878472c7311c6414a7e5a149e0179fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:54:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'app DMZ wuxuu bixiyaa qalab brannmur nidaamyada aaga de-millatariyeysan ah. Takooro adeegyada sare dibadda halis sida web, e-mail iyo nidaamka lagu ladhay loo hagaajin karo ammaanka ee network.';
+$lang['dmz_app_name'] = 'DMZ Brannmur';
+$lang['dmz_incoming_connection'] = 'DMZ Connection socda';
+$lang['dmz_incoming_connections'] = 'DMZ Connections socda';
+$lang['dmz_network_not_configured'] = 'interface A network la door qoondeeyey ee DMZ waa la waayay.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ in LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ in LAN) Connections';
