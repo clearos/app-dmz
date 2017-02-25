fa9cce2db2fd7dae33110059273f4bf5f480379c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:55:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ app ger brandväggs verktyg för system i de paramilitära zon. Isolera högrisk externa tjänster såsom webb, e-post och VoIP-system kan förbättra säkerheten i ditt nätverk.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ inkommande anslutning';
+$lang['dmz_incoming_connections'] = 'DMZ inkommande anslutningar';
+$lang['dmz_network_not_configured'] = 'Ett nätverksgränssnitt med en utsedd roll DMZ kunde inte hittas.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ till LAN)';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ till LAN) Anslutningar';
