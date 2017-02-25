39085027b8fffc30cb90efbd193d8f194ce81410
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:50:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ App DE-әскерилендірілген аймағында жүйелер үшін брандмауэр құралдарын ұсынады. Мұндай веб, электрондық пошта және VoIP жүйесі ретінде жоғары тәуекел сыртқы қызметтерді оқшаулау сіздің желінің қауіпсіздігін жетілдіруге болады.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Кіріс қосылу';
+$lang['dmz_incoming_connections'] = 'DMZ Кіріс Қосылымдар';
+$lang['dmz_network_not_configured'] = 'DMZ тағайындалған рөлі бар желілік интерфейс табу мүмкін емес.';
+$lang['dmz_pinhole_connection'] = 'DMZ Пинхол (DMZ Жергілікті) қосылу';
+$lang['dmz_pinhole_connections'] = 'DMZ Пинхол (DMZ Жергілікті) Қосылымдар';
