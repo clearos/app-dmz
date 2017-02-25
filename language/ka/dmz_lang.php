7bdba597b50b3e28f20daaee04c5e38759328820
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:50:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ app უზრუნველყოფს firewall ინსტრუმენტები სისტემების დემილიტარიზებულ ზონაში. იზოლაცია მაღალი რისკის გარე მომსახურება, როგორიცაა ინტერნეტში, ელექტრონული ფოსტით და VoIP სისტემას შეუძლია გააუმჯობესოს უსაფრთხოების თქვენი ქსელი.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ შემომავალი კავშირი';
+$lang['dmz_incoming_connections'] = 'DMZ შემომავალი კავშირები';
+$lang['dmz_network_not_configured'] = 'ქსელის ინტერფეისი დანიშნული როლი DMZ ვერ მოიძებნა.';
+$lang['dmz_pinhole_connection'] = 'DMZ ნახვრეტში (DMZ to LAN) კავშირი';
+$lang['dmz_pinhole_connections'] = 'DMZ ნახვრეტში (DMZ to LAN) კავშირი';
