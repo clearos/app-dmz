e4d2eba60a7fba3620508a75b4d49c8d7816318f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:51:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ അപ്ലിക്കേഷൻ ദേ-സൈനികവൽക്കരിക്കപ്പെട്ട മേഖലയിൽ സിസ്റ്റങ്ങൾക്കും ഫയർവാൾ ഉപകരണങ്ങൾ നൽകുന്നു. പോലുള്ള വെബ്, ഇ-മെയിൽ, VoIP സിസ്റ്റം ഉയർന്ന റിസ്ക് ബാഹ്യ സേവനങ്ങൾ വേർതിരിച്ചിരുന്ന നിങ്ങളുടെ നെറ്റ്വർക്ക് സുരക്ഷ മെച്ചപ്പെടുത്താൻ കഴിയും.';
+$lang['dmz_app_name'] = 'DMZ ഫയർവാൾ';
+$lang['dmz_incoming_connection'] = 'DMZ ഇൻകമിംഗ് കണക്ഷൻ';
+$lang['dmz_incoming_connections'] = 'DMZ ഇൻകമിംഗ് കണക്ഷനുകൾ';
+$lang['dmz_network_not_configured'] = 'DMZ ഒരു നിയുക്ത പദവിയുള്ള നെറ്റ്വർക്ക് ഇന്റർഫേസ് കണ്ടെത്താൻ കഴിഞ്ഞില്ല.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ ലാൻ ലേക്ക്) കണക്ഷൻ';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ ലാൻ ലേക്ക്) കണക്ഷനുകൾ';
