62fb85ae4dc553db70fc188c63334d0fbb8c9f70
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:51:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'стан на DMZ заштитниот ѕид обезбедува алатки за системи во зоната де-милитаризирана. Изолирање висок надворешни услуги на ризик како што се VoIP систем за веб, е-пошта и да се подобри безбедноста на вашата мрежа.';
+$lang['dmz_app_name'] = 'Firewall DMZ';
+$lang['dmz_incoming_connection'] = 'DMZ Доаѓаат Поврзување';
+$lang['dmz_incoming_connections'] = 'DMZ дојдовни конекции';
+$lang['dmz_network_not_configured'] = 'А мрежа интерфејс со одредена улога на DMZ не може да се најде.';
+$lang['dmz_pinhole_connection'] = 'DMZ точкест отвор (DMZ да LAN) конекција';
+$lang['dmz_pinhole_connections'] = 'DMZ точкест отвор (DMZ на LAN) Поврзувања';
