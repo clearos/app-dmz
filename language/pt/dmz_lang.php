8ef1b8127fe0bc804242b83b6fb9d18afe2b8f81
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:53:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'O aplicativo DMZ fornece ferramentas de firewall para sistemas na zona desmilitarizada. Isolando serviços de alto risco externos, tais como web, e-mail e sistema de VoIP pode melhorar a segurança de sua rede.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ conexão de entrada';
+$lang['dmz_incoming_connections'] = 'DMZ conexões de entrada';
+$lang['dmz_network_not_configured'] = 'A interface de rede com um papel designado de DMZ não pôde ser encontrado.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ para LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ pinhole (DMZ para LAN) Conexões';
