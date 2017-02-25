fb814230ecf6968e93933bcdddbec6371a31b0bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:48:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Η εφαρμογή DMZ παρέχει τείχος προστασίας εργαλείων για τα συστήματα στην αποστρατικοποιημένη ζώνη. Απομόνωση υψηλού εξωτερικές υπηρεσίες κινδύνου, όπως web, e-mail και το σύστημα VoIP μπορεί να βελτιώσει την ασφάλεια του δικτύου σας.';
+$lang['dmz_app_name'] = 'DMZ Τείχος προστασίας';
+$lang['dmz_incoming_connection'] = 'DMZ εισερχόμενη σύνδεση';
+$lang['dmz_incoming_connections'] = 'DMZ εισερχόμενες συνδέσεις';
+$lang['dmz_network_not_configured'] = 'Μια διεπαφή δικτύου με ένα καθορισμένο ρόλο του DMZ δεν θα μπορούσε να βρεθεί.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ σε LAN) Σύνδεση';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ σε LAN) Συνδέσεις';
