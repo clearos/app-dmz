d1b7ad9ef179efa5670b921717ffcfcbc4e69d96
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:52:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ अनुप्रयोग डी-militarized झोन मध्ये प्रणाली फायरवॉल साधने पुरवतो. ही वेब, ई-मेल आणि VoIP प्रणाली म्हणून उच्च धोका बाह्य सेवा हरवून आपल्या नेटवर्क सुरक्षा सुधारण्यासाठी करू शकता.';
+$lang['dmz_app_name'] = 'DMZ फायरवॉल';
+$lang['dmz_incoming_connection'] = 'DMZ येणार्या कनेक्शन';
+$lang['dmz_incoming_connections'] = 'DMZ येणार्या जोडण्या';
+$lang['dmz_network_not_configured'] = 'DMZ एक नियुक्त भूमिका नेटवर्क संवाद सापडू शकला नाही.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (लॅन करण्यासाठी DMZ) कनेक्शन';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ लान पर्यंत) जोडण्या';
