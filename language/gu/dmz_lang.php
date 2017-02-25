4ec771ad9e50ac6c5fc171faa2207a56c45acd6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:48:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ એપ્લિકેશન દ-લશ્કરીકરણ ઝોનમાં સિસ્ટમો માટે ફાયરવોલ સાધનો પૂરા પાડે છે. જેમ કે વેબ, ઈ-મેલ અને વીઓઆઈપી સિસ્ટમ તરીકે ઊંચા જોખમ બાહ્ય સેવાઓ અલગ તમારા નેટવર્ક સુરક્ષા સુધારી શકે છે.';
+$lang['dmz_app_name'] = 'DMZ ફાયરવોલ';
+$lang['dmz_incoming_connection'] = 'DMZ ઇનકમિંગ જોડાણ';
+$lang['dmz_incoming_connections'] = 'DMZ આવતા જોડાણો';
+$lang['dmz_network_not_configured'] = 'DMZ એક નિયુક્ત ભૂમિકા સાથે નેટવર્ક ઈન્ટરફેસ નથી મળી શકે છે.';
+$lang['dmz_pinhole_connection'] = 'DMZ pinhole (LAN માટે DMZ) જોડાણ';
+$lang['dmz_pinhole_connections'] = 'DMZ pinhole (DMZ લાન માટે) જોડાણો';
