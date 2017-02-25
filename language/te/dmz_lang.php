8c6200ed6bafa2f69a473c4ec6a6b324846266e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:55:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'DMZ అనువర్తనం డి-సైన్యికరించబడి జోన్ వ్యవస్థళ్లొ ఫైర్వాల్ టూల్స్ అందిస్తుంది. వంటి వెబ్, ఇ-మెయిల్ మరియు VoIP వ్యవస్థ అధిక ప్రమాదం బాహ్య సేవలు వేర్పాటు మీ నెట్వర్కు యొక్క భద్రతా మెరుగుపరచడానికి.';
+$lang['dmz_app_name'] = 'DMZ ఫైర్వాల్';
+$lang['dmz_incoming_connection'] = 'DMZ ఇన్కమింగ్ కనెక్షన్';
+$lang['dmz_incoming_connections'] = 'DMZ ఇన్కమింగ్ కనెక్షన్లు';
+$lang['dmz_network_not_configured'] = 'DMZ యొక్క ఒక నియమించబడిన పాత్ర తో ఒక నెట్వర్క్ ఇంటర్ఫేస్ కనపడుటలేదు.';
+$lang['dmz_pinhole_connection'] = 'DMZ చిల్లు (తో LAN DMZ) కనెక్షన్';
+$lang['dmz_pinhole_connections'] = 'DMZ చిల్లు (DMZ తో LAN) కనెక్షన్లు';
