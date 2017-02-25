775723d0456ce8f3bfd13653eb8e70c7f9b137e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:51:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Ny fampiharana DMZ Firewall manome fitaovana ho an&#39;ny rafitra eo amin&#39;ny de-ireo tafika faritra. Mitokantokana atahorana tolotra any ivelany toy ny tranonkala, ny e-mail sy ny VoIP rafitra afaka hanatsarana ny fiarovana ny tambajotra.';
+$lang['dmz_app_name'] = 'DMZ Firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Incoming Connection';
+$lang['dmz_incoming_connections'] = 'DMZ Incoming Connections';
+$lang['dmz_network_not_configured'] = 'A tambajotra voatendry interface tsara amin&#39;ny anjara asan&#39;ny DMZ tsy ho hita.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (DMZ ny LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (DMZ ny LAN) Connections';
