3bd4d2c3a855a02fb1f26339eed18eaac25d3b95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:57:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/dmz_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['dmz_app_description'] = 'Uhlelo lokusebenza okungavunyelwe ukulwa kuyo inikeza amathuluzi firewall izinhlelo zone de-elihlomile. Ukuzihlukanisa okusezingeni ingozi amasevisi angaphandle njenge web, e-mail futhi VoIP uhlelo kungenza ngcono ukulondeka inethiwekhi yakho.';
+$lang['dmz_app_name'] = 'DMZ firewall';
+$lang['dmz_incoming_connection'] = 'DMZ Connection olungenayo';
+$lang['dmz_incoming_connections'] = 'DMZ Connections olungenayo';
+$lang['dmz_network_not_configured'] = 'A interface inethiwekhi indima eziqokiwe DMZ alikwazanga ukutholwa.';
+$lang['dmz_pinhole_connection'] = 'DMZ Pinhole (okungavunyelwe ukulwa kuyo ukuze LAN) Connection';
+$lang['dmz_pinhole_connections'] = 'DMZ Pinhole (okungavunyelwe ukulwa kuyo ukuze LAN) Connections';
